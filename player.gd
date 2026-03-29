4f5a5d83376afe7ca286bf107dabd28ceca9183b
Mauro Melender <mauromelender2@gmail.com>
2026-03-29 18:13:27 -0300

    Ta mal

--- player.gd
+++ player.gd
@@ -25,7 +25,6 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
 
 func play_animation(anim_name: String):
