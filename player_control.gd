2be654828a1ce96e155fbf05041ac88aa633c02e
Mauro Melender <mauromelender2@gmail.com>
2026-03-29 16:47:50 -0300

    PlayerControl

--- player_control.gd
+++ player_control.gd
@@ -1,9 +1,9 @@
 extends Node
 
-@onready var player:Player = self.owner 
+# El Player tenia que tener: class_name Player
+@onready var player: Player = self.owner 
 
-
-var gravity:float = ProyectSettings.get_settings("physics/2d/default_gravity")
+var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 enum STATE {
 	IDLE,
@@ -12,29 +12,36 @@ enum STATE {
 	CROUCHED
 }
 
-var current_state:STATE = STATE.IDLE
+var current_state: STATE = STATE.IDLE
 
-func _physics_process(delta)
+func _physics_process(delta: float): 
 	match current_state:
-		STATE.IDLE
+		STATE.IDLE: 
 			player.velocity.x = 0
 			player.play_animation(player.animation.idle)
-			if Imput.is_action_pressed("ui_left") or Imput.is_action_pressed("ui_right"):
+			if Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_right"):
 				current_state = STATE.RUNNING
-			elif Imput.is_action_pressed("ui_up"):
-				current.state = STATE.JUMPING
-		STATE.RUNNING
-			player.velocity.x = Imput.get_axis("ui_left" , "ui_right") + player.movement_stats.running_speed
+			elif Input.is_action_just_pressed("ui_up"):
+				current_state = STATE.JUMPING
+
+		STATE.RUNNING:
+			player.velocity.x = Input.get_axis("ui_left", "ui_right") + player.movement_stats.running_speed
 			player.play_animation(player.animation.running)
-			if Imput.is_action_pressed("ui_up"):
-				current.state = STATE.JUMPING
-			if not Imput.is_action_pressed("ui_left") and not Imput.is_action_pressed("ui_right"):
-				current_state = STATE.RUNNING
-		STATE.JUMPING
-			player.velocity.x = player.movement_stats.jump_speed
+			
+			if Input.is_action_pressed("ui_up"):
+				current_state = STATE.JUMPING	
+			elif not Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right"):
+				current_state = STATE.IDLE 
+
+		STATE.JUMPING:
 			if player.is_on_floor():
 				current_state = STATE.IDLE
-		STATE.CROUCHED
+		
+		STATE.CROUCHED:
 			pass
-	handle_gravity(delta)
-	12	
+			
+	player.move_and_slide() 
+
+func handle_gravity(delta):
+	if not player.is_on_floor():
+		player.velocity.y += gravity * delta
